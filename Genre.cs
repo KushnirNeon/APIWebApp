8dc86bcc87a2d75d5cb835f90d51a9a13801c6d3
NeonKushnir <52037326+KushnirNeon@users.noreply.github.com>
2025-11-11 23:10:25 +0200

    Add files via upload

---
+++ Genre.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace APIWebApp.Models;
+
+public partial class Genre
+{
+    public Genre()
+    {
+        Books = new List<Book>();
+    }
+    public int id { get; set; }
+
+    public string name { get; set; }
+
+    public string description { get; set; }
+
+    public virtual ICollection<Book> Books { get; set; }
+}
---
+++ Publisher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace APIWebApp.Models;
+
+public partial class Publisher
+{
+    public Publisher()
+    {
+        PublishersAuthors = new List<PublishersBook>();
+    }
+    public int id { get; set; }
+
+    public string name { get; set; }
+
+    public virtual ICollection<PublishersBook> PublishersAuthors { get; set; }
+}
